e4189754e9006add0db124dae79e008db7639d6d
emkuepper <ekuepper@hotmail.com>
2024-05-28 12:32:03 -0400

    added tslearn dtw

--- src/gait_metrics.py
+++ src/gait_metrics.py
@@ -1,5 +1,7 @@
 # functions for calculating different gait scores
 import numpy as np
+import statistics as stats
+from tslearn.metrics import dtw_path
 
 '''
 Calculates the Gait Profile Score (GPS) as outlined by Baker et al. (https://pubmed.ncbi.nlm.nih.gov/19632117/)
@@ -16,15 +18,15 @@ Inputs:
 '''
 def calc_gait_profile_score(part_kinematics, control_kinematics):
     # organize kinematics into single array
-    def create_kin_profile(kinematics_dict)
+    def create_kin_profile(kinematics_dict):
         kinematics_arr = []
         kinematics_arr.append(np.mean(part_kinematics['pelvis_orient'], axis=0).tranpose())
         kinematics_arr.append(np.mean(part_kinematics['hip_angle'][0], axis = 0).tranpose())
         kinematics_arr.append(np.mean(part_kinematics['hip_angle'][1], axis = 0).tranpose())
-        kinematics_arr.append(np.expand_dims(np.mean(part_kinemaics['knee_angle'][0,:,:,2], axis = 0), axis=0))
-        kinematics_arr.append(np.expand_dims(np.mean(part_kinemaics['knee_angle'][1,:,:,2], axis = 0), axis=0))
-        kinematics_arr.append(np.expand_dims(np.mean(part_kinemaics['ankle_angle'][0,:,:,2], axis = 0), axis=0))
-        kinematics_arr.append(np.expand_dims(np.mean(part_kinemaics['ankle_angle'][1,:,:,2], axis = 0), axis=0))
+        kinematics_arr.append(np.expand_dims(np.mean(part_kinematics['knee_angle'][0,:,:,2], axis = 0), axis=0))
+        kinematics_arr.append(np.expand_dims(np.mean(part_kinematics['knee_angle'][1,:,:,2], axis = 0), axis=0))
+        kinematics_arr.append(np.expand_dims(np.mean(part_kinematics['ankle_angle'][0,:,:,2], axis = 0), axis=0))
+        kinematics_arr.append(np.expand_dims(np.mean(part_kinematics['ankle_angle'][1,:,:,2], axis = 0), axis=0))
         
         kinematics_arr = np.concatenate(kinematics_arr, axis=0)
         return kinematics_arr
@@ -32,13 +34,40 @@ def calc_gait_profile_score(part_kinematics, control_kinematics):
     
     # mean kinematics arrays. Should be shape [Number of signals, 51 time points]
     part_mean_kinematics = create_kin_profile(part_kinematics)
-    control_mean_kinematics = creat_kin_profile(control_kinematics)
+    control_mean_kinematics = create_kin_profile(control_kinematics)
     
     
-    gait_variability_score = [np.linalg.norm(part_mean_kinmatics[i] - control_mean_kinematics[i]) / np.sqrt(len(part_mean_kinematics[i]))
+    gait_variability_score = [np.linalg.norm(part_mean_kinematics[i] - control_mean_kinematics[i]) / np.sqrt(len(part_mean_kinematics[i]))
                                  for i in range(len(part_mean_kinematics))]
     
     gait_profile_score = np.linalg.norm(gait_variability_score) / np.sqrt(len(gait_variability_score))
     
     return gait_profile_score
-    
+
+
+""" TSLEARN DTW - documentation: https://tslearn.readthedocs.io/en/stable/gen_modules/metrics/tslearn.metrics.dtw_path.html#tslearn.metrics.dtw_path
+Inputs:
+    set1, set2 (optional): List containing (sz,D) arrays, where D is the dimension of the data, sz is the length of the gait cycle
+Returns:
+    Mean DTW distance between partitioned gait cycles - if set 1 and set 2 are inputs, the DTW distance between datasets is computed. If set 1 is an input, the within DTW distance is computed.
+"""
+def tslearn_dtw_analysis(set1, set2=None):
+        def tslearndtw_distance(data1, data2):
+            path, distance = dtw_path(data1.astype(np.double), data2.astype(np.double))
+            return distance
+        dtw_dist_within = []; dtw_dist_between = []
+        #If only one dataset is provided, will compute the DTW distances within the partitioned gait cycle data.
+        if set2 is None:
+            for i in range(len(set1)):
+                for j in range(i + 1, len(set1)):
+                    distance = tslearndtw_distance(set1[i], set1[j])
+                    dtw_dist_within.append(distance)
+            return {'Mean DTW distances': stats.mean(dtw_dist_within)}
+        #Computes the DTW distances between gait cycles between 2 different files.
+        else:
+            for gaitcycle1 in set1:
+                for gaitcycle2 in set2:
+                    distance = tslearndtw_distance(gaitcycle1, gaitcycle2)
+                    dtw_dist_between.append(distance)
+            return {'Mean DTW distances': stats.mean(dtw_dist_between)}
+        
