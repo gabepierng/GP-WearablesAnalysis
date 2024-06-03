cc1474334bbe493df4902435b726b42eb3b92d60
emkuepper <ekuepper@hotmail.com>
2024-06-03 17:25:43 -0400

    added SOM and DTW implementation

--- src/gait_metrics.py
+++ src/gait_metrics.py
@@ -1,6 +1,8 @@
 # functions for calculating different gait scores
 import numpy as np
 from tslearn.metrics import dtw_path
+from minisom import MiniSom
+import matplotlib.pyplot as plt
 
 '''
 Calculates the Gait Profile Score (GPS) as outlined by Baker et al. (https://pubmed.ncbi.nlm.nih.gov/19632117/)
@@ -44,9 +46,12 @@ def calc_gait_profile_score(part_kinematics, control_kinematics):
     return gait_profile_score
 
 
-""" TSLEARN DTW - documentation: https://tslearn.readthedocs.io/en/stable/gen_modules/metrics/tslearn.metrics.dtw_path.html#tslearn.metrics.dtw_path
-Inputs: set1, set2 (optional): List containing (sz,D) arrays as elements, where D is the dimension of the data, sz is the length of the gait cycle 
-Returns: Mean DTW distance between partitioned gait cycles - if set 1 and set 2 are inputs, the DTW distance between datasets is computed. If set 1 is an input, the within DTW distance is computed.
+""" 
+TSLEARN DTW - documentation: https://tslearn.readthedocs.io/en/stable/gen_modules/metrics/tslearn.metrics.dtw_path.html#tslearn.metrics.dtw_path
+Parameters: 
+    set1, set2 (List): List containing (sz,D) arrays as elements, where D is the dimension of the data, sz is the length of the gait cycle 
+Returns: 
+    Mean DTW distance between partitioned gait cycles - if set 1 and set 2 are inputs, the DTW distance between datasets is computed. If set 1 is an input, the within DTW distance is computed.
 """
    
 def tslearn_dtw_analysis(set1, set2=None):
@@ -69,3 +74,62 @@ def tslearn_dtw_analysis(set1, set2=None):
                 dtw_dist.append(distance)
         return np.mean(dtw_dist)
 
+
+"""
+Training a Self Organizing Map (SOM)
+
+Parameters:
+    control_data (numpy array): The input data for training the SOM. Dimensions NxD, where D is the dimension of the data 
+    learning_rate (float): The initial learning rate for the SOM.
+    topology (str): The topology of the SOM ('rectangular' or 'hexagonal').
+Returns:
+    som: Trained SOM
+"""
+
+def train_minisom(control_data, learning_rate=0.1, topology='hexagonal'):
+ 
+    # Parameters for training/initializing: 
+    dim = int(np.sqrt(5 * np.sqrt(control_data.shape[0]))) #Heuristic: # map units = 5*sqrt(n) [1]
+    som_dim = (dim, dim)
+    steps = 500 * (dim ** 2) #Number of iterations - Heuristic: 500 * number of network units [2] 
+    sigma = dim/4 #Sigma used: Heuristic: max(msize)/4 [1]
+    
+    som = MiniSom(x=som_dim[0], y=som_dim[1], input_len=control_data.shape[1], sigma=sigma, learning_rate=learning_rate, topology=topology) #Initializing the SOM 
+    
+    som.random_weights_init(control_data) #Initializes the weights of the SOM picking random samples from data.
+    som.train_batch(control_data, steps) #Trains the SOM using all the vectors in data sequentially.
+
+    # Calculate the dimensions of the SOM and training steps
+    dim = int(np.sqrt((5 * np.sqrt(control_data.shape[0]))))
+    som_dim = (dim, dim)
+    steps = 500 * (dim ** 2)
+    
+    # Initialize the MiniSom
+    som = MiniSom(x=som_dim[0], y=som_dim[1], input_len=control_data.shape[1], sigma=dim/4, learning_rate=learning_rate, topology=topology)
+    
+    # Randomly initialize weights and train the SOM
+    som.random_weights_init(control_data)
+    som.train_batch(control_data, steps)
+    
+    """    
+    Resources
+    [1]	    Juha. Vesanto and (Libella), SOM toolbox for Matlab 5. Helsinki University of Technology, 2000.
+    [2]	    S. Lek and Y. S. Park, “Artificial Neural Networks,” Encyclopedia of Ecology, Five-Volume Set, vol. 1–5, pp. 237–245, Jan. 2008, doi: 10.1016/B978-008045405-4.00173-7.
+    
+    """
+    return som
+
+"""    
+Calculates Mean MDP scores for the given data and trained SOM.
+Parameters:
+    data (numpy array): The input data for which Mean MDP scores are calculated.
+    som (MiniSom): The trained SOM.
+Returns:
+    deviation: Euclidian distance between BMU and dataset for each time point -- used to generate MDP
+"""
+
+def calculate_MDP(data, som):
+    winners = np.array([som.winner(instance) for instance in data])  # Finds the best matching unit (BMU) for all time points in the data
+    BMU = som._weights[winners[:, 0], winners[:, 1]]  # Collects the corresponding weight vectors for all BMUs
+    deviation = np.linalg.norm(data - BMU, axis=1)  # Calculate Euclidean distances in the full dataset
+    return deviation
